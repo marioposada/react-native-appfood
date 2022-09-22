755faf2a13d9aa5e3d4928c4465135b9cbbd12bd
Mario Posada <marioposadamiranda@gmail.com>
2022-09-22 13:32:28 -0300

    Add FlatList

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,11 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 
 import React from 'react'
 
-const ResultsList = ({title}) => {
+const ResultsList = ({title,results}) => {
 return (
     <View>
         <Text style={style.title}>{title}</Text>
+        <FlatList 
+        horizontal
+        data={results}
+        keyExtractor={(result) =>  result.id}
+        renderItem={({item}) => {
+    return <Text>{item.name}</Text>
+    }}
+        
+        
+        
+        />
     </View>
 )    
 }
